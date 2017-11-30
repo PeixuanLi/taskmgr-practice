305fd00d30c123f0764efcc7ae6971ec98d480aa
PeixuanLi <pl1284@nyu.edu>
2017-11-30 01:08:31 -0600

    add dynamic form for login component

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  form:FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email : new FormControl('info@peixuanli.com',Validators.compose([Validators.email, Validators.required])),
+      password : new FormControl('',Validators.required)
+    });
+  }
+  onSubmit({value,valid}, ev: Event){
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(valid);
   }
 
 }
